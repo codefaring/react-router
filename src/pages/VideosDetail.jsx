940336e96d64bf9eca1192d9788ea06e4c4b02a2
codefaring <codefaring@gmail.com>
2023-03-03 14:04:37 +0900

    react router & outlet

---
+++ src/pages/VideosDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+export default function VideosDetail() {
+  const { videoId } = useParams();
+  return (
+    <div>
+      VideosDetail <p>{videoId}</p>
+    </div>
+  );
+}
